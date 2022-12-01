a56580e619841c33c92137e879bae01cda5d54d5
Denis <dvkulikovv@mail.ru>
2022-12-01 17:07:01 +0300

    Добавили задачку с поиском индекса элемента массива

---
+++ Ex012/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {12, 22, 36, 45, 57, 62, 12, 75, 84};
+
+int n = array.Length;
+int find = 12;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1
+    index++;
+};
