afc862c8868f3ef2116765d3fd0e9cd95d03ce8f
Denis <dvkulikovv@mail.ru>
2022-12-01 17:45:21 +0300

    Добавили метод заполнения и распечатывания массива

---
+++ Ex013/Program.cs
@@ -0,0 +1,29 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;  
+    }
+
+}
+
+void PrintArray(int[] petro)
+{
+    int count = petro.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(petro[position]);
+        position++;
+    }
+}
+
+int[] array = new int [10];
+
+FillArray(array);
+PrintArray(array);
+
+
